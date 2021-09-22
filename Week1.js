bef7ea27e8a7c7282d3785082dfbe66855579647
jeff1haupt <jeff.lawjmh@gmail.com>
2021-09-21 21:26:04 -0400

    My First Commit

---
+++ Week1.js
@@ -0,0 +1,28 @@
+// Week 1 - 6 JavaScript 
+
+let x = 50; 
+
+let y = x * x; 
+
+let z = y * x; 
+
+let firstName = '';
+
+firstName = "Jeff";
+console.log(firstName)
+firstName = 142; 
+console.log(firstName)
+firstName = true; 
+console.log(firstName)
+firstName = null;
+console.log(firstName)
+
+
+
+firstName // camel Case - use this for JavaScript
+
+PersonStudent // starts uppercase - only for class names 
+
+first_name // snake case - may see this in databases 
+
+
